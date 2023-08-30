bb70be52a8045124ef96d0e2fe0ac3c602c5d4f5
Eugene Vetrov <evg.vetrow@yandex.ru>
2023-08-30 20:50:14 +0500

    add subscription section to footer

--- components/global/Footer.vue
+++ components/global/Footer.vue
@@ -7,12 +7,13 @@ footer(class="footer-app")
         div(class="footer-app__content_right")
             FooterContacts
             FooterSocialIcons
+    FooterSubscription
 </template>
 
 <style lang="scss">
     .footer-app {
         display: flex;
-        flex-wrap: nowrap;
+        flex-wrap: wrap;
         width: 100%;
         background-color: $white;
         justify-content: center;
@@ -84,6 +85,62 @@ footer(class="footer-app")
             &-icons {
                 margin-top: 30px;
             }            
+        }
+
+        &__subscription {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            padding: 0 5%;
+            margin-top: 100px;
+            min-height: 100px;
+            background-color: $black;
+            width: 70%;
+            max-width: 1340px;
+
+            @media screen and (max-width: 1440px) {
+                width: 100%;
+            }
+
+            &-text {
+                color: $white;
+                font-size: 22px;
+                font-weight: 800;
+                line-height: 30px;
+            }
+
+            &-form {
+                display: flex;
+                flex-wrap: nowrap;
+                align-items: flex-end;
+                margin: 20px 0;
+
+                @media screen and (max-width: 540px) {
+                    flex-wrap: wrap;
+                }
+            }
+
+            &-input {
+                background-color: $black;
+                border: none;
+                border-bottom: 1px solid $gray;
+                margin-right: 37px;
+                height: 28px;
+            }
+
+            &-button {
+                background-color: $blue;
+                color: $white;
+                border-radius: 2px;
+                border: none;
+                width: 223px;
+                height: 46px;
+
+                @media screen and (max-width: 540px) {
+                    margin-top: 20px;
+                }
+            }
         }
     }
 </style>
