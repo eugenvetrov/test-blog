96301d310df9a78d77ff03ee8059552edd5441e6
Eugene Vetrov <evg.vetrow@yandex.ru>
2023-09-05 18:48:34 +0500

    add text block component

--- components/article/TextBlock.vue
+++ components/article/TextBlock.vue
@@ -1,11 +1,55 @@
 <script setup lang="ts">
-    import DOMPurify from 'dompurify'
+    import DOMPurify from 'isomorphic-dompurify'
+    
     const props = defineProps(["textData"])
     const textData = props.textData
-    console.log(textData);
-    
+    const clean = DOMPurify.sanitize(textData)
 </script>
 
 <template lang="pug">
-p(v-html="textData")
+div(class="text-block")
+    div(v-html="clean")
+    nuxt-img(src="images/svg/quotes-left.svg" class="text-block__image_quote")
 </template>
+
+<style lang="scss">
+    .text-block {
+        position: relative;
+
+        & p {
+            font-size: 18px;
+            line-height: 26px;
+            color: $eerie-black;
+        }
+
+        & blockquote {
+            margin-left: 130px;
+            font-size: 18px;
+            line-height: 26px;
+            font-weight: 300;
+            color: $gray;
+            :first-child {
+                color: $gray;
+                font-style: italic;
+            }
+            @media screen and (max-width: 899px) {
+                    & {
+                        margin-left: 0px;
+                    }
+                }
+        }
+
+        &__image {
+            &_quote {
+                position: absolute;
+                bottom: 60px;
+                @media screen and (max-width: 899px) {
+                    & {
+                        position: relative;
+                        bottom: -10px;
+                    }
+                }
+            }
+        }
+    }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,5 +34,5 @@ export default defineNuxtConfig({
     prerender: {
       crawlLinks: true,
     },
-  },
+  }
 })
