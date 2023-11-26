58a312a3756d3bdd909e0077467905fd3c36cb39
Eugene Vetrov <evg.vetrow@yandex.ru>
2023-11-26 13:44:33 +0500

    change default back

--- stores/base.ts
+++ stores/base.ts
@@ -4,16 +4,16 @@ export const useBaseStore = defineStore('page', () => {
     const articleData = ref<IBaseData<IData>>()
     const fetchHomeData = async () => {
         const isEnvApiUrl = config.public?.apiUrl === undefined
-        const apiUrl = isEnvApiUrl ? config.public.apiUrl : "https://devtwit8.ru/api/v1/"
+        const apiUrl = isEnvApiUrl ? config.public.apiUrl : "http://127.0.0.1:8000/"
         const { data } =
-            await useFetch<IBaseData<IData>>(`${apiUrl}page/?path=/`)
+            await useFetch<IBaseData<IData>>(`${apiUrl}`)
         if (data.value) homeData.value = data.value
     }
     const fetchArticleData = async (articleId: string) => {
         const isEnvApiUrl = config.public?.apiUrl === undefined
-        const apiUrl = isEnvApiUrl ? config.public.apiUrl : "https://devtwit8.ru/api/v1/"
+        const apiUrl = isEnvApiUrl ? config.public.apiUrl : "http://127.0.0.1:8000/"
         const { data } =
-            await useFetch<IBaseData<IData>>(`${apiUrl}page/?path=/article-${articleId}`)
+            await useFetch<IBaseData<IData>>(`${apiUrl}articles/${articleId}`)
         if (data.value) articleData.value = data.value
     }
     return { homeData,
